bd283db4dca1fd8a8196328c8daec7362ca10dc8
Marisa Vatanya <mvatanya@gmail.com>
2019-05-28 14:10:31 -0700

    modified js instead of putting just back images, now put both front and back images in the tiles and fixed layout

--- script.js
+++ script.js
@@ -1,30 +1,57 @@
 window.addEventListener("DOMContentLoaded", () => {
-    function drawTiles(){
-        var imagesContainer = document.getElementById("images-container");
-        for (var j = 0; j < 5; j++) {
-            var rowDiv = document.createElement("div");
-            rowDiv.className = "row" 
-            for (var i = 0; i < 5; i++){
-                var columnDiv = document.createElement("div");
-                columnDiv.className = "column"
-                if(j === 2 && i === 2){
-                    var countNum = document.createElement("span");
-                    countNum.className = "countNum";
-                    countNum.textContent = 0;
-                    columnDiv.appendChild(countNum);
-                } else {
-                    var backImage = document.createElement("img");
-                    backImage.className = "backImage";
-                    backImage.src = "back-image.jpg";  
-                    columnDiv.appendChild(backImage); 
-                }
-                rowDiv.appendChild(columnDiv); 
+    // function drawTiles(){
+    //     var imagesContainer = document.getElementById("images-container");
+    //     for (var j = 0; j < 5; j++) {
+    //         var rowDiv = document.createElement("div");
+    //         rowDiv.className = "row" 
+    //         for (var i = 0; i < 5; i++){
+    //             var columnDiv = document.createElement("div");
+    //             columnDiv.className = "column"
+    //             if(j === 2 && i === 2){
+    //                 var countNum = document.createElement("span");
+    //                 countNum.className = "countNum";
+    //                 countNum.textContent = 0;
+    //                 columnDiv.appendChild(countNum);
+    //             } else {
+    //                 var backImage = document.createElement("img");
+    //                 backImage.className = "backImage";
+    //                 backImage.src = "back-image.jpg";  
+    //                 columnDiv.appendChild(backImage); 
+    //             }
+    //             rowDiv.appendChild(columnDiv); 
           
+    //         }
+    //         imagesContainer.appendChild(rowDiv);
+    //     }
+    // }
+    // drawTiles();
+
+    function cardTiles(){
+        var frontImageFiles = ["gif-1.webp","gif-1.webp","gif-2.webp","gif-2.webp","gif-3.webp","gif-3.webp","gif-4.webp","gif-4.webp","gif-5.webp","gif-5.webp","gif-6.webp","gif-6.webp","","gif-7.webp","gif-7.webp","gif-8.webp","gif-8.webp","gif-9.webp","gif-9.webp","gif-10.webp","gif-10.webp","gif-11.webp","gif-11.webp","gif-12.webp","gif-12.webp"];
+        var cardsContainer = document.getElementById("cardsContainer");
+        for (var i = 0; i < 25; i++) {
+            var memCards = document.createElement("div");
+            memCards.className = "memCards" 
+            if(i === 12){
+                var countNum = document.createElement("span");
+                countNum.className = "countNum";
+                countNum.textContent = 0;
+                memCards.appendChild(countNum);
+            } else {
+                var frontImage = document.createElement("img");
+                frontImage.className = "frontImage" 
+                frontImage.src = frontImageFiles[i];
+                var backImage = document.createElement("img");
+                backImage.className = "backImage";
+                backImage.src = "back-image.jpg"; 
+                memCards.appendChild(frontImage); 
+                memCards.appendChild(backImage); 
             }
-            imagesContainer.appendChild(rowDiv);
+        cardsContainer.appendChild(memCards);    
         }
-    }
-    drawTiles();
+    };
+    
+    cardTiles();
     var count = 0;
     var imagesContainerElement =  document.getElementById("images-container");
     imagesContainerElement.addEventListener("click", function(event){
@@ -37,8 +64,6 @@ window.addEventListener("DOMContentLoaded", () => {
         var showCountNumber = document.getElementsByClassName("countNum")[0]
         showCountNumber.textContent = count;
     })
-
-
     
 })    
 
